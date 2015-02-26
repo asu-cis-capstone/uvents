57dc2d5c839b51c92991d915597c1f51d7638dfb
ptamang1 <ptamang1@asu.edu>
2015-02-25 18:16:14 -0700

    Boostrap
    Used bootstrap to make a lot of front end changes.

---
+++ CIS440/testpage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<!-- 
+Home Page
+-->
+
+<html lang="en">
+  	
+  <head>
+    <!-- Meta tag -->
+	<meta name= "robots" content="noindex,no follow" />
+    <meta charset= "utf-8" />
+	
+	<!-- This meta tag allows the mobile version on mobile, tablet on tablet, desktop on desktop -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+    <!-- Link tag for CSS -->
+    <link type="text/css" rel="stylesheet" href="stylesheets/uss.css" />
+	<link href = "bootstrap/css/bootstrap.min.css" rel = "stylesheet">
+	<link href = "bootstrap/css/styles.css" rel = "stylesheet">
+	
+	<!-- Javascript tags -->
+	<script type="text/javascript" src="js/messages.js"></script>
+	<script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src = "bootstrap/js/bootstrap.js"></script>
+	
+	<!-- Favicon tag --> 
+	<link rel="icon" href="images/blueplane.png"/>
+	
+    <!-- Web Page Title -->
+    <title>Home Page</title>
+	
+  </head>
+
+  <body>
+	<div class = "navbar navbar-default navbar-static-top">
+		<div class = "container">
+		
+			<div = "#" class = "navbar-brand">Uvents</div>
+			<div class = "collapse navbar-collapse navHeaderCollapse">
+				<ul class = "nav navbar-nav navbar-right">
+					<li><a href = "#">About Us</a></li>
+				
+				</ul>		
+			</div>
+		</div>
+	</div>
+  	</body>
+</html>
+
