efca2856f0bb1606d54487f97874836eadc4349b
DelgadoJorge <jldelgad@asu.edu>
2015-03-25 00:43:55 -0700

    createed category pages and fixed reference links

--- flat-theme/events/eventlist.php
+++ flat-theme/events/eventlist.php
@@ -12,20 +12,20 @@ Events Searched
     <meta name="description" content="">
     <meta name="author" content="">
     <title>uvents</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/font-awesome.min.css" rel="stylesheet">
+    <link href="../css/prettyPhoto.css" rel="stylesheet">
+    <link href="../css/animate.css" rel="stylesheet">
+    <link href="../css/main.css" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
 	
 		<!-- This meta tag allows the mobile version on mobile, tablet on tablet, desktop on desktop -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -42,14 +42,14 @@ Events Searched
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">uvents</a>
+                <a class="navbar-brand" href="../index.html">uvents</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="index.html">Home</a></li>
-                    <li><a href="#l">Create</a></li>
-                    <li><a href="#">Clubs</a></li>
-                    <li><a href="#">Categories</a></li>
+                    <li class="active"><a href="../index.html">Home</a></li>
+                    <li class="active"><a href="../events.php">Events</a></li>
+                    <li><a href="../clubs.htm">Clubs</a></li>
+                    <li><a href="../categories.html">Categories</a></li>
                 </ul>
             </div>
         </div>
@@ -62,7 +62,7 @@ Events Searched
 	{
 
 		//Call on this file to connect to database
-		include('local-connect.php');
+		include('../local-connect.php');
 
 		//Define variables
 		$criteria = $_POST['criteria'];
