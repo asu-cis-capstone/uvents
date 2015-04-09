4da362dc61a7634f33fbe73c8d65394bde841d16
StevenLie <slie2@asu.edu>
2015-04-09 00:20:05 -0700

    Logo Header
    logo header change in 7 pages in categories

--- flat-theme/categories/bars.php
+++ flat-theme/categories/bars.php
@@ -37,7 +37,9 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="../index.html">uvents</a>
+                <a class="navbar-brand" href="index.html">
+				<img class="img-responsive" src="images/LogoDone.png" width = "130" height = "75"alt="">
+				</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
